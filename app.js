6d5f94a38bba2b26b46392e7c62da4c5c964b734
Manali Shetye <manalis826@gmail.com>
2021-05-18 23:09:40 +0530

    added mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const path = require("path");
+const mongoose = require('mongoose');
+const User = require('./models/index');
 
 const app = express();
 app.use(express.static(path.join(__dirname, "static")));
@@ -8,6 +11,22 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+mongoose.connect(process.env.DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(()=>{
+    //we write app.listen here so that the server runs only after dB is connected
+    const port = process.env.PORT || 80;
+    app.listen(port, ()=>{
+    console.log(`server is running at port ${port}`);
+});
+
+
+}).catch((err)=>{
+    console.log(err);
+});
 
 app.get('/', (req, res)=>{
     res.status(200).render('index');
@@ -18,8 +37,3 @@ app.get('/', (req, res)=>{
 
 
 
-const port = process.env.PORT || 80;
-
-app.listen(port, ()=>{
-    console.log(`server is running at port ${port}`);
-});

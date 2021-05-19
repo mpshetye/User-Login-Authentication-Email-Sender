4a5480d05a4e9c171f8fd212d70fb9240e811a17
Manali Shetye <manalis826@gmail.com>
2021-05-19 12:55:49 +0530

    added routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const path = require("path");
-const mongoose = require('mongoose');
-const User = require('./models/index');
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+// const User = require('./models/userModel'); dot is root directory... to move out from another directory to root use two dots and then slash.
 
 const app = express();
 app.use(express.static(path.join(__dirname, "static")));
@@ -12,28 +13,25 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-mongoose.connect(process.env.DB_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).then(()=>{
+mongoose
+  .connect(process.env.DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
     //we write app.listen here so that the server runs only after dB is connected
     const port = process.env.PORT || 80;
-    app.listen(port, ()=>{
-    console.log(`server is running at port ${port}`);
-});
-
-
-}).catch((err)=>{
+    app.listen(port, () => {
+      console.log(`server is running at port ${port}`);
+    });
+  })
+  .catch((err) => {
     console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.status(200).render("index");
 });
 
-app.get('/', (req, res)=>{
-    res.status(200).render('index');
-});
-
-
-
-
-
-
+app.use(userRoutes);

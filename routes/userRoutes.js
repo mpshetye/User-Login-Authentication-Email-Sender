a7555be0d6d6ec9cd935f9729383d1d78e46eda5
Manali Shetye <manalis826@gmail.com>
2021-05-21 00:10:33 +0530

    added controllers to routers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,37 +1,9 @@
 const router = require("express").Router();
-const User = require("../models/userModel");
+const userController = require('../controllers/userController');
 
-router.post("/register", (req, res) => {
-  const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  if (password === confirmPassword) {
-    const registerUser = new User(req.body);
-    registerUser
-      .save()
-      .then((data) => {
-        res.send(data);
-        // res.redirect('/');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    res.send("password not matching");
-  }
-});
 
-router.post("/login", async (req, res) => {
-  try {
-    const password = req.body.password;
-    const userDetails = await User.findOne({ userName: req.body.userName });
-    if (userDetails.password == password) {
-      res.send("log in successful");
-    } else {
-      res.send("Invalid");
-    }
-  } catch (err) {
-    res.status(400).send("Invalid User");
-  }
-});
+router.post("/register", userController.register_user);
+
+router.post("/login", userController.login_user);
 
 module.exports = router;

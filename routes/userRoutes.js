cfee0c91e3f4f0411e8d89b287cc709101fa688d
Manali Shetye <manalis826@gmail.com>
2021-06-09 01:03:15 +0530

    added routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const userController = require('../controllers/userController');
 
 
+router.get("/register", userController.register_get);
 router.post("/register", userController.register_user);
 
+router.get("/login", userController.login_get);
 router.post("/login", userController.login_user);
 
 module.exports = router;

b309c3744ff465acf593e2a1c5988a4e4f184070
Manali Shetye <manalis826@gmail.com>
2021-06-09 01:03:29 +0530

    jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,76 @@
 const User = require("../models/userModel");
-const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+
+//handling errors
+const handleErrors = (err)=>{
+  console.log(err.message, err.code);
+  
+  let errors = {firstName:``,
+    lastName:``,
+    email:``,
+    phone:``,
+    userName:``,
+    age:``,
+    gender:``,
+    password:``};
+
+    //duplicate error handling
+    if(err.code === 11000){
+      errors.email = `that email is already registered`
+      errors.phone = `that phone is already registered`
+      errors.userName = `that user name is already registered`
+
+      return errors;
+    }
+
+    //validation errors
+    if (err.message.includes('User validation failed')){
+      Object.values(err.errors).forEach(({properties}) =>{
+        errors[properties.path] = properties.message;
+      });
+    }
+
+    return errors
+
+}
+
+
+const maxAge = 30 * 60;
+const createToken = (id) =>{
+  return jwt.sign({id}, process.env.SECRET_KEY, {
+    expiresIn: maxAge
+  });
+}
+
+const register_get = (req, res) =>{
+  res.render('register');
+}
 
 const register_user = async (req, res) => {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   if (password === confirmPassword) {
-    const registerUser = new User(req.body);
-    registerUser.password = await bcrypt.hash(registerUser.password, 10);
-    registerUser
-      .save()
-      .then((data) => {
-        res.send(data);
-        // res.redirect('/');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try{
+      const registerUser = await User.create(req.body) ;
+      const token = createToken(registerUser._id);
+      res.cookie('jToken', token, {httpOnly: true, maxAge: maxAge * 1000});
+      res.status(200).json({user: registerUser._id});
+    }
+    catch(err){
+      const errorMessage = handleErrors(err);
+      console.log(errorMessage);
+      res.status(400).json({errorMessage});
+    }
   } else {
     res.send("password not matching");
   }
 };
+
+
+const login_get = (req, res) =>{
+  res.render('login');
+}
 
 
 const login_user = async (req, res) => {
@@ -40,6 +90,8 @@ const login_user = async (req, res) => {
 
 
 module.exports = {
+  register_get,
   register_user,
+  login_get,
   login_user,
 };

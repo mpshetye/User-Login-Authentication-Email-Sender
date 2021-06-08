6b52fe9d1df096f1e09963ee0493f717fd295e16
Manali Shetye <manalis826@gmail.com>
2021-06-09 01:02:43 +0530

    added pre save

--- models/userModel.js
+++ models/userModel.js
@@ -1,40 +1,48 @@
 const Schema = require('mongoose').Schema;
 const {model} = require('mongoose');
+const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const registerSchema = new Schema({
     firstName:{
         type:String,
-        required:true
+        required:[true, `Please enter your First Name`]
     },
     lastName:{
         type:String,
-        required:true
+        required:[true, `Please enter your Last Name`]
     }, 
     email:{
         type:String,
-        required:true,
-        unique: true
+        required:[true, `Please enter an email`],
+        unique: true,
+        lowercase: true,
+        validate:[isEmail,`Please enter a valid email`]
     },
     phone:{
         type:Number,
-        required:true,
-        unique: true
+        required:[true, `Please enter a phone number`],
+        unique: true,
+        // min: [10, `Please enter correct phone number`],
+        // max: [11, `Please enter correct phone number`]
     },
     userName:{
         type:String,
-        required:true,
+        required:[true, `Please enter user name`],
         unique: true
     },
     age:{
         type:Number,
-        required:true
+        required:[true, `Please enter your age`]
     },
     gender:{
         type:String,
-        required: true
+        required: [true, `Please choose gender`]
     },
     password:{
-        type:String
+        type:String,
+        required: [true, `Please enter password`],
+        minlength: [8, `Minimum password length is 8 characters`]
     },
     // confirmPassword:{
     //     type: String
@@ -42,6 +50,11 @@ const registerSchema = new Schema({
 
 
 });
+
+registerSchema.pre('save', async function(next){
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = model('User', registerSchema);
 

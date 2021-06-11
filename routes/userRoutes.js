2b5182b3b47f1216776248804d69e68135902398
Manali Shetye <manalis826@gmail.com>
2021-06-12 00:49:19 +0530

    added logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,6 @@ router.post("/register", userController.register_user);
 router.get("/login", userController.login_get);
 router.post("/login", userController.login_user);
 
+router.get("/logout", userController.logout_get);
+
 module.exports = router;

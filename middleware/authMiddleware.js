fc82dda5329916e8507e5516f91d8031b0662594
Manali Shetye <manalis826@gmail.com>
2021-06-13 04:05:08 +0530

    middleware function to check if user is signed in

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 const isAuth = (req, res, next) =>{
     const token = req.cookies.acjt;
@@ -20,4 +21,29 @@ const isAuth = (req, res, next) =>{
     }
 }
 
+const checkUser = (req, res, next) =>{
+    const token = req.cookies.acjt;
+
+    if(token){
+        jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken)=>{
+            if(err){
+                console.log(err.message);
+                res.locals.data = null;
+                next();
+            }
+            else{
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.data = user;
+                next();
+            }
+        });
+    }
+    else{
+        res.locals.data = null;
+        next();
+    }    
+}
+
 module.exports.isAuth = isAuth;
+module.exports.checkUser = checkUser;

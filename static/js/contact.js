454ad08484793d8ae09c19d4b73f81cfa3f3dc3d
Manali Shetye <manalis826@gmail.com>
2021-06-22 21:32:32 +0530

    initial commit

---
+++ static/js/contact.js
@@ -0,0 +1,48 @@
+$('#success').hide();
+const form = document.querySelector("form");
+// const userError = document.querySelector("#userNameHelp");
+// const passwordError = document.querySelector("#logPasswordHelp");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  //   // reset errors
+  //   userError.textContent = "";
+  //   passwordError.textContent = "";
+  //   // get values
+  const firstName = form.firstName.value;
+  const lastName = form.lastName.value;
+  const email = form.email.value;
+  const subject = form.subject.value;
+  const messageArea = form.messageArea.value;
+  let bodyData = {
+    firstName,
+    lastName,
+    email,
+    subject,
+    messageArea,
+  };
+  try {
+    const res = await fetch("/help/contact_page", {
+      method: "POST",
+      body: JSON.stringify(bodyData),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    console.log(data.msg);
+    // console.log(data.errorMessages);
+    //     if (data.errorMessages) {
+    //       userError.textContent = data.errorMessages.userName;
+    //       passwordError.textContent = data.errorMessages.password;
+    //     }
+    if (data.msg) {
+      // location.assign("/help/contact_page");
+      $('#success').show();
+      $('#alertPara').text(data.msg);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});

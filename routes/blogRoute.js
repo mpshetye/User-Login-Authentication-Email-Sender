42bced7046ec96d2a8e288d21fea595ce1a23c64
Manali Shetye <manalis826@gmail.com>
2021-06-25 13:32:04 +0530

    initial commit

---
+++ routes/blogRoute.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const blogController = require('../controllers/blogController');
+
+router.get('/blogs', blogController.blog_get);
+
+router.get('/create', blogController.addblog_get);
+router.post('/create', blogController.addblog_post);
+
+// router.get('/create', blogController.blog_get);
+// router.get('/create', blogController.blog_get);
+
+module.exports = router;

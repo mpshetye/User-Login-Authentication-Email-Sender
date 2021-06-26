00a0d471d34594b6ff43e78ebecf917855a3daa0
Manali Shetye <manalis826@gmail.com>
2021-06-26 22:18:47 +0530

    added route for reading the blog content

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -6,6 +6,9 @@ router.get('/blogs', blogController.blog_get);
 router.get('/create', blogController.addblog_get);
 router.post('/create', blogController.addblog_post);
 
+router.get('/read/:id', blogController.readblog_get);
+
+
 // router.get('/create', blogController.blog_get);
 // router.get('/create', blogController.blog_get);
 

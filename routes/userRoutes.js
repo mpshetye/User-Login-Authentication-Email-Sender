a2dfce0bf39f21efaed7c376836bd448bf494b54
Manali Shetye <manalis826@gmail.com>
2021-06-27 21:39:33 +0530

    changed userController function names

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,10 @@ const userController = require('../controllers/userController');
 
 
 router.get("/register", userController.register_get);
-router.post("/register", userController.register_user);
+router.post("/register", userController.register_user_post);
 
 router.get("/login", userController.login_get);
-router.post("/login", userController.login_user);
+router.post("/login", userController.login_user_post);
 
 router.get("/logout", userController.logout_get);
 

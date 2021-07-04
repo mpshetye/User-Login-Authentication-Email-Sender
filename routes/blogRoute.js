f96b775c5b817ca95e0fb5bd9d4b214327a2e895
Manali Shetye <manalis826@gmail.com>
2021-07-04 22:14:55 +0530

    added edit and delete routes

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,15 +1,25 @@
 const router = require('express').Router();
 const blogController = require('../controllers/blogController');
+const authMiddleware = require('../middleware/authMiddleware');
+
 
 router.get('/blogs', blogController.blog_get);
 
-router.get('/create', blogController.addblog_get);
-router.post('/create', blogController.addblog_post);
 
-router.get('/read/:id', blogController.readblog_get);
+router.get('/create', authMiddleware.isAuth, blogController.addblog_get);
+router.post('/create',authMiddleware.isAuth, blogController.addblog_post);
 
 
-// router.get('/create', blogController.blog_get);
-// router.get('/create', blogController.blog_get);
+router.get('/read/:id', authMiddleware.isAuth, blogController.readblog_get);
+
+
+router.get('/edit-delete-blogs', authMiddleware.isAuth, blogController.editDeleteBlog_get);
+
+
+router.get('/edit-blogs/:id',authMiddleware.isAuth, blogController.editBlog_get);
+router.post('/edit-blogs/:id',authMiddleware.isAuth, blogController.editBlog_post);
+
+router.delete('/delete-blogs/:id',authMiddleware.isAuth, blogController.deleteBlog_delete);
+
 
 module.exports = router;

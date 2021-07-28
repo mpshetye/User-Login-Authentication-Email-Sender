efd790bc3425c72fb76c2e3bbb3cf449763597ca
Manali Shetye <manalis826@gmail.com>
2021-07-28 12:01:46 +0530

    added form help

--- static/js/addBlog.js
+++ static/js/addBlog.js
@@ -1,4 +1,45 @@
 const form = document.querySelector("form");
+const TitleError = document.querySelector("#blogTitleHelp");
+const SnippetError = document.querySelector("#blogSnippetHelp");
+const BodyError = document.querySelector("#blogBodyHelp");
+const blogTitleElem = form.blogTitle;
+const blogSnippetElem = form.blogSnippet;
+const blogBodyElem = form.blogBody;
+
+
+
+blogTitleElem.addEventListener("blur", (e) => {
+  TitleError.textContent = ''
+  const blogTitleA = blogTitleElem.value;
+  let regex = /^\s/;
+  if (blogTitleA == "" || regex.test(blogTitleA)) {
+    TitleError.textContent = 'Enter a valid Title for the Blog';
+  }
+});
+
+
+blogSnippetElem.addEventListener("blur", (e) => {
+  SnippetError.textContent = ''
+  const blogSnippetA = blogSnippetElem.value;
+  let regex = /^\s/;
+  if (blogSnippetA == "" || regex.test(blogSnippetA)) {
+    SnippetError.textContent = 'Enter a valid Snippet for the Blog';
+  }
+});
+
+
+
+blogBodyElem.addEventListener("blur", (e) => {
+  BodyError.textContent = ''
+  const blogBodyA = blogBodyElem.value;
+  let regex = /^\s/;
+  if (blogBodyA == "" || regex.test(blogBodyA)) {
+    BodyError.textContent = 'Enter a valid Body for the Blog';
+  }
+});
+
+
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const blogFirstName = form.blogFirstName.value;
@@ -30,10 +71,10 @@ form.addEventListener("submit", async (e) => {
     });
     const data = await res.json();
     console.log(data);
-    if(data.blog){
-        location.assign("/nodeblogapi/blogs");
+    if (data.blog) {
+      location.assign("/nodeblogapi/blogs");
     }
-  } catch(err) {
-      console.log(err);
+  } catch (err) {
+    console.log(err);
   }
 });
